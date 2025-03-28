cff665d43b6ab793be5e88b24a9710278272fce5
IkayImonoko <ntolok1990@gmail.com>
2025-03-28 19:35:11 +0100

    +new user; all id start from 1 insted of 0

--- utilities/model.js
+++ utilities/model.js
@@ -9,33 +9,34 @@ const model = {
     },
     data: {
         users: [
-            { id: 0, levelId: 0, name: 'Nikita', email: 'nikita@gmail.com', password: '123' },
-            { id: 1, levelId: 0, name: 'Sigrid', email: 'sigrid@gmail.com', password: '456' },
+            { id: 1, levelId: 1, name: 'Nikita', email: 'nikita@gmail.com', password: '123' },
+            { id: 2, levelId: 1, name: 'Sigrid', email: 'sigrid@gmail.com', password: '456' },
+            { id: 3, levelId: 1, name: 'Peter', email: 'peter@gmail.com', password: '789' },
         ],
         levels: [
-            { id: 0, name: 'Level 1' },
-            { id: 1, name: 'Level 2' },
+            { id: 1, name: 'Level 1' },
+            { id: 2, name: 'Level 2' },
         ],
         words: [
-            { id: 0, russian: 'привет', norwegian: 'hi' },
-            { id: 1, russian: 'столяр', norwegian: 'snekker' }
+            { id: 1, russian: 'привет', norwegian: 'hi' },
+            { id: 2, russian: 'столяр', norwegian: 'snekker' }
         ],
         levelsContents: [
-            { id: 0, livelId: 0, wordId: 0 },
-            { id: 1, livelId: 0, wordId: 1 },
+            { id: 1, livelId: 1, wordId: 1 },
+            { id: 2, livelId: 1, wordId: 2 },
         ],
         progresses: [
-            { id: 0, name: 'First view' },
-            { id: 1, name: 'Started' },
-            { id: 2, name: 'In progress' },
-            { id: 3, name: 'Repitition' },
-            { id: 4, name: 'Lerned' }
+            { id: 1, name: 'First view' },
+            { id: 2, name: 'Started' },
+            { id: 3, name: 'In progress' },
+            { id: 4, name: 'Repitition' },
+            { id: 5, name: 'Lerned' }
         ],
         usersProgresses: [
-            { id: 0, userId: 0, wordId: 0, progressId: 4 },
-            { id: 1, userId: 0, wordId: 1, progressId: 4 },
-            { id: 2, userId: 1, wordId: 0, progressId: 1 },
-            { id: 3, userId: 1, wordId: 1, progressId: 1 },
+            { id: 1, userId: 1, wordId: 1, progressId: 4 },
+            { id: 2, userId: 1, wordId: 2, progressId: 4 },
+            { id: 3, userId: 2, wordId: 1, progressId: 1 },
+            { id: 4, userId: 3, wordId: 1, progressId: 1 },
         ]
     }
 }
