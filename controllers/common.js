539382afb3708b99d64bba11d8f1c0bd4d269218
IkayImonoko <ntolok1990@gmail.com>
2025-04-24 15:40:05 +0200

    move get functions in common

--- controllers/common.js
+++ controllers/common.js
@@ -1,3 +1,29 @@
 function getUser(id) {
     return model.data.users.find(x => { return x.id === id });
 }
+
+function getWord(id) {
+    for (const word of model.data.words) {
+        if (word.id === id) {
+            return word;
+        }
+    }
+}
+
+function getPogress(id) {
+    for (const progress of model.data.progresses) {
+        if (progress.id === id) {
+            return progress;
+        }
+    }
+}
+
+function getUserLevel(id) {
+    const user = model.data.users.find(x => x.id === id);
+    return user ? user.levelId : null;
+}
+
+function getLevelName(id) {
+    const level = model.data.levels.find(x => x.id === id);
+    return level ? level.name : '';
+}
--- controllers/userpageController.js
+++ controllers/userpageController.js
@@ -1,29 +1,3 @@
-function getWord(id) {
-    for (const word of model.data.words) {
-        if (word.id === id) {
-            return word;
-        }
-    }
-}
-
-function getPogress(id) {
-    for (const progress of model.data.progresses) {
-        if (progress.id === id) {
-            return progress;
-        }
-    }
-}
-
-function getUserLevel(id) {
-    const user = model.data.users.find(x => x.id === id);
-    return user ? user.levelId : null;
-}
-
-function getLevelName(id) {
-    const level = model.data.levels.find(x => x.id === id);
-    return level ? level.name : '';
-}
-
 function handleLogin(){
     console.log('onclick');
 }
