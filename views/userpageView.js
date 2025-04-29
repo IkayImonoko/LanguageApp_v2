3d600d9e6000e1cf7eb19a892ee5f24a7e99c14c
IkayImonoko <ntolok1990@gmail.com>
2025-04-29 22:30:36 +0200

    delete commented code

--- views/userpageView.js
+++ views/userpageView.js
@@ -1,24 +1,9 @@
 function userpageView() {
     let currentUser = model.data.users.find(user => user.id === model.inputs.userpage.userId);
-    // for (const user of model.data.users) {
-    //     if (user.id === model.inputs.userpage.userId) {
-    //         currentUser = user;
-    //         break
-    //     }
-    // }
 
     let tableRows = '';
     //let level = currentUser.levelId;
     if (currentUser) {
-        // for (const event of model.data.events) {
-        //     if (event.userId === currentUser.id) {
-        //         let word = '';
-        //         let progress = '';
-        //         word = getWord(event.wordId);
-        //         progress = getPogress(event.progressId);
-        //         tableRows += `<tr><td>${word.russian}/${word.norwegian}</td><td>${progress.name}</td></tr>`
-        //     }
-        // }
         tableRows = model.data.events.reduce((result, event) => {
             if (event.userId === currentUser.id) {
                 return result + `<tr><td>${getWord(event.wordId).russian}/${getWord(event.wordId).norwegian}</td><td>${getPogress(event.progressId).name}</td></tr>`
