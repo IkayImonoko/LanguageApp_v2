54bcba4006d6a99e1d4f3a4232ab8a5a07810d53
IkayImonoko <ntolok1990@gmail.com>
2025-08-19 21:37:11 +0200

    add input on frontend

--- Front/views/wordsBaseViev.js
+++ Front/views/wordsBaseViev.js
@@ -1,21 +1,36 @@
 function wordsBaseView() {
 
-    var wordPairsHTML = `
+    var wordPairsHTML = /*HTML*/`
     <div class="table-wrapper">
         <table>
             <thead>
                 <tr>
                 <th>Russian</th>
                 <th>Norwegian</th>
+                <th>Category</th>
                 </tr>
             </thead>
             <tbody id="wordTableBody">
     `;
-    wordPairsHTML += model.data.words ? model.data.words.map((w) => ` <tr><td>${w.russian}</td> <td>${w.norwegian}</td></tr>`).join('') : '';
-    wordPairsHTML += `
+    wordPairsHTML += model.data.words ? model.data.words.map((w) => ` <tr><td>${w.russian}</td> <td>${w.norwegian}</td> <td>${w.category}</td></tr>`).join('') : '';
+    wordPairsHTML += /*HTML*/`
         </tbody>
         </table>
         </div>
     `;
+
+    var addNewWordHTML = /*HTML*/`
+    <div>
+        <label for ="ruInput">Russian</label>
+        <input id="ruInput">
+        <label for ="noInput">Norvegian</label>
+        <input id="noInput">
+        <label for ="categoryInput">Category</label>
+        <input id="categoryInput">
+    </div>
+    <button>New word</button>
+    `;
+    wordPairsHTML += addNewWordHTML;
+
     return wordPairsHTML;
 }
